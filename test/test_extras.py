15b2216e18d80799157ec0cf5504809ea9155c52
James Percival <j.percival@imperial.ac.uk>
2017-05-23 15:15:13 +0100

    Add test

--- test/test_extras.py
+++ test/test_extras.py
@@ -2,3 +2,12 @@ import pytest
 
 def test_import():
     import vtk_extras
+
+
+def test_read():
+    from vtk_extras import ReadGmsh
+    import os
+    
+    os.chdir("test")
+    ugrid = ReadGmsh("box.msh")
+    assert(ugrid.GetNumberOfPoints()>0)
